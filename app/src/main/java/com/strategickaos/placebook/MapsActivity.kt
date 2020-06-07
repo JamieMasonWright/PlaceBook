127820323396403616fb838b73f4ba27bea65639
Jamie Mason_Wright <jamie.mason-wright@lush.co.uk>
2020-06-07 19:43:28 +0100

    enabled correct map markers and live location tracking

--- app/src/main/java/com/strategickaos/placebook/MapsActivity.kt
+++ app/src/main/java/com/strategickaos/placebook/MapsActivity.kt
@@ -1,28 +1,24 @@
 package com.strategickaos.placebook
 
 import android.content.pm.PackageManager
-import android.nfc.Tag
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.activity_maps.*
-import java.util.jar.Manifest
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback
 {
 
 	private lateinit var mMap: GoogleMap
 	private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
 
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
@@ -35,15 +31,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
 		setUpLocationClient()
 	}
 
-	/**
-	 * Manipulates the map once available.
-	 * This callback is triggered when the map is ready to be used.
-	 * This is where we can add markers or lines, add listeners or move the camera. In this case,
-	 * we just add a marker near Sydney, Australia.
-	 * If Google Play services is not installed on the device, the user will be prompted to install
-	 * it inside the SupportMapFragment. This method will only be triggered once the user has
-	 * installed Google Play services and returned to the app.
-	 */
 	override fun onMapReady(googleMap: GoogleMap)
 	{
 		mMap = googleMap
@@ -72,11 +59,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback
 				PackageManager.PERMISSION_GRANTED){
 			requestLocationPermission()
 		}else{
-			fusedLocationProviderClient.lastLocation.addOnCompleteListener{
+				mMap.isMyLocationEnabled = true
+				fusedLocationProviderClient.lastLocation.addOnCompleteListener{
 				val location = it.result
 				if(location != null){
 					val latLng = LatLng(location.latitude, location.longitude)
-					mMap.addMarker(MarkerOptions().position(latLng).title("You are here"))
 					val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
 					mMap.moveCamera(update)
 				}else{
